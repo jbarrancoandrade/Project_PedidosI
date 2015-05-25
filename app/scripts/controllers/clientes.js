fa6eedf2813a378b0cbd326e88e79ac6931b6473
jhon barranco <patronjk7@gmail.com>
2015-05-25 16:46:29 -0500

    ejecutando

--- app/scripts/controllers/clientes.js
+++ app/scripts/controllers/clientes.js
@@ -3,38 +3,34 @@
 
 angular.module('projectPedidosApp')
   .controller('ClientesCtrl',['$scope','clientes','$mdDialog','Auth', function ($scope, clientes, $mdDialog,Auth) {
-    clientes.get(function (response) {
-      $scope.clientes = response.results;
-    });
 
 
+    /*$scope.cargar = function (client) {
+      $scope.newCliente = client;
+    };*/
 
-    $scope.cargar = function(ev,cliente) {
+    $scope.clientes = clientes.get();
+
+    $scope.diaogNewUser = function(ev,x) {
       $mdDialog.show({
-        controller: ['$scope','$mdDialog', 'client' ,function ($scope, $mdDialog, client) {
-          console.log(client);
+        controller: ['$scope', '$mdDialog', 'client', '$location', function ($scope, $mdDialog, client, $location) {
+
           $scope.newCliente = client;
-          $scope.hide = function () {
-            $mdDialog.hide();
-          };
+
           $scope.cancel = function () {
             $mdDialog.cancel();
           };
-          $scope.answer = function (answer) {
-            $mdDialog.hide(answer);
+          $scope.continuar = function () {
+            $location.path('/Almacenes');
+            $mdDialog.hide();
           };
         }],
         templateUrl: 'views/dialogs/dialogo_cliente.html',
         targetEvent: ev,
-        locals: {client: cliente},
+        locals: {client: x},
         bindToController: true
-      })
-        .then(function(answer) {
-          $scope.alert = 'You said the information was "' + answer + '".';
-        }, function() {
-          $scope.alert = 'You cancelled the dialog.';
-        });
-    };
+      });
+    }
 
     $scope.logout = function () {
       $scope.dataLoading = false;
